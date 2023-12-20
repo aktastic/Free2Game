2fc8234d4605664e274c2552989dc690bb58200e
aktastic <arkulac@gmail.com>
2023-12-20 09:58:09 +0100

    Sidebar changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import "./Nav.scss"
+import "./Nav.scss";
+import lupeIcon from "../img/lupe.png";
 
 const Nav = () => {
   // useState für Klick auf Searchbar-Container (div)
@@ -7,28 +8,31 @@ const Nav = () => {
 
   // Funktion: Ändere den state (false --> true / true --> false) bei klick auf den Searchbar-Container (div)
   const changeBorderOnClick = () => {
-    setOnClickBorder((borderColor) => !borderColor)
-  }
+    setOnClickBorder((borderColor) => !borderColor);
+  };
 
   return (
     <>
-    <section className="topnav_container">
+      <section className="topnav_container">
+        {/* Searchbar */}
+        <form role="search">
+          <div className={onClickBorder ? " border_focus" : ""}>
+            {/* Search Input-Field */}
+            <input
+              type="search"
+              name="searchInput"
+              id="searchInput"
+              placeholder="Search..."
+              onClick={changeBorderOnClick}
+            />
 
-    {/* Searchbar */}
-    <form role="search">
-      <div  className={onClickBorder ? " border_focus" : ""} >
-        
-        {/* Search Input-Field */}
-        <input type="search" name="searchInput" id="searchInput" placeholder="Search..." onClick={changeBorderOnClick}/>
-
-        {/* Search Button/Magnifying glass */}
-        <button><img src="/lupe.png" alt="Lupe Icon" /></button>
-        </div>
-    </form>
-
-    </section>
-    
-      
+            {/* Search Button/Magnifying glass */}
+            <button>
+              <img src={lupeIcon} alt="Lupe Icon" />
+            </button>
+          </div>
+        </form>
+      </section>
     </>
   );
 };
