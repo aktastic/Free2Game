ea0a27f4f1deabd2d4d221c68a142ec72abf3589
trisi99 <trisi.buschi@gmail.com>
2023-12-20 14:24:49 +0100

    navigate te details

--- src/components/ButtonFilled.jsx
+++ src/components/ButtonFilled.jsx
@@ -1,7 +1,9 @@
+import { Link, useNavigate } from "react-router-dom";
 import "./ButtonFilled.scss";
 
-const ButtonFilled = ({ name }) => {
-  return <button className="buttonFilled">{name}</button>;
+const ButtonFilled = ({ name , navigateFunc }) => {
+  const navigate = useNavigate()
+  return <button onClick={navigateFunc} className="buttonFilled">{name}</button>;
 };
 
 export default ButtonFilled;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,26 +3,28 @@ import ButtonOutline from "./ButtonOutline";
 import "./Card.scss";
 import Vector from "../img/Vector.svg";
 import Group from "../img/Group.svg";
+import { useNavigate } from "react-router-dom";
 
 const Card = (props) => {
+  const navigate = useNavigate()
   return (
     <>
       <div className="Card__Design">
         <img src={props.thumbnail} alt="" />
         <article>
           <h2>{props.title}</h2>
-          <ButtonFilled name="READ MORE" />
+          <ButtonFilled navigateFunc={()=>navigate(`/details/${props.id}`)} name="READ MORE" />
           {/* <hr /> */}
           {props.platform === "PC (Windows), Web Browser" ? (
             <div className="Bottom_Btns">
-              <ButtonOutline name={<img src={Vector} alt="Windows Logo" />} />
-              <ButtonOutline name={<img src={Group} alt="Browser Logo" />} />
-              <ButtonOutline name={props.genre} />
+              <ButtonOutline filterfunc={props.filterfunc3} name={<img src={Vector} alt="Windows Logo" />} />
+              <ButtonOutline filterfunc={props.filterfunc4} name={<img src={Group} alt="Browser Logo" />} />
+              <ButtonOutline name={props.genre} filterfunc={props.filterfunc2}/>
             </div>
           ) : (
             <div className="Bottom_Btns">
-              <ButtonOutline name={<img src={props.svg} />} />
-              <ButtonOutline name={props.genre} />
+              <ButtonOutline filterfunc={props.filterfunc1} name={<img src={props.svg} />} />
+              <ButtonOutline name={props.genre} filterfunc={props.filterfunc2}/>
             </div>
           )}
         </article>
