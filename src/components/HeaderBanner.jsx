cdd61394a16dc43d13b7248e233ec1b675226ab6
huangrunhong <huang.run.hong.1986@gmail.com>
2023-12-21 16:12:47 +0100

    Header commponent

--- src/components/HeaderBanner.jsx
+++ src/components/HeaderBanner.jsx
@@ -1,13 +1,10 @@
 import "./HeaderBanner.scss";
-const HeaderBanner = () => {
+const HeaderBanner = (props) => {
   return (
     <>
       <div className="header_banner" name="top">
-        <img
-          src="https://media.discordapp.net/attachments/1186291209308741684/1186332027457392710/Gruppenbild.jpg?ex=6592dccc&is=658067cc&hm=9d3c25cca16df8ee336ac05851db8ecb20752037c35b49b77e3d85f6b3ad71f8&=&format=webp&width=1588&height=794"
-          alt="home_header_image"
-        />
-        <h1>Find & track the best free-to-play games!</h1>
+        <img src={props.url} alt="header_image" />
+        <h1>{props.title}</h1>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,10 @@ const Home = () => {
   console.log(recentlyAddedGames);
   return (
     <section className="HomePage">
-      <HeaderBanner />
+      <HeaderBanner
+        url="https://media.discordapp.net/attachments/1186291209308741684/1186332027457392710/Gruppenbild.jpg?ex=6592dccc&is=658067cc&hm=9d3c25cca16df8ee336ac05851db8ecb20752037c35b49b77e3d85f6b3ad71f8&=&format=webp&width=1588&height=794"
+        title="Find & track the best free-to-play games!"
+      />
       <main>
         <section className="recentlyAdded">
           <h2>Recently Added</h2>
