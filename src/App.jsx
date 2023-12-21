2792bd254289786acb6a86ab979708f51806560a
aktastic <arkulac@gmail.com>
2023-12-21 10:16:07 +0100

    details fetch implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   AllGamesContext,
   PopularityContext,
   RelevanceContext,
+  DetailsContext,
 } from "./context/FetchContext";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
@@ -12,17 +13,18 @@ import RecentlyAdded from "./pages/RecentlyAdded";
 import FetchAllGames from "./data/FetchAllGames";
 import FetchPopularity from "./data/FetchPopularity";
 import FetchRelevance from "./data/FetchRelevance";
+import FetchDetails from "./data/FetchDetails";
 
 import Nav from "./components/Nav";
 import Menu from "./components/Menu";
 
 import "./App.scss";
 
-
 function App() {
   const [allGames, setAllGames] = useState([]);
   const [popularityGames, setPopularityGames] = useState([]);
   const [relevanceGames, setRelevanceGames] = useState([]);
+  const [detailsOfGames, setDetailsOfGames] = useState([]);
 
   return (
     <>
@@ -33,22 +35,27 @@ function App() {
           <RelevanceContext.Provider
             value={{ relevanceGames, setRelevanceGames }}
           >
-            <FetchAllGames />
-            <FetchPopularity />
-            <FetchRelevance />
-            <BrowserRouter>
-            {/* <Nav />
-            <Menu />     */}
+            <DetailsContext.Provider
+              value={{ detailsOfGames, setDetailsOfGames }}
+            >
+              <FetchAllGames />
+              <FetchPopularity />
+              <FetchRelevance />
+              <FetchDetails />
+              <BrowserRouter>
+                <Nav />
+                <Menu />
 
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/allgames" element={<AllGames />} />
-                <Route path="/recentlyadded" element={<RecentlyAdded />} />
-                {/* Test Details */}
-                <Route path="/details" element={<Details />}/>
-              </Routes>
-            </BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/details/:id" element={<Details />} />
+                  <Route path="/allgames" element={<AllGames />} />
+                  <Route path="/recentlyadded" element={<RecentlyAdded />} />
+                  {/* Test Details */}
+                  <Route path="/details" element={<Details />} />
+                </Routes>
+              </BrowserRouter>
+            </DetailsContext.Provider>
           </RelevanceContext.Provider>
         </PopularityContext.Provider>
       </AllGamesContext.Provider>
